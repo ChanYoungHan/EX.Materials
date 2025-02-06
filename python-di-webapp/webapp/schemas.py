82d6f60b34762146af1443cf3d3600bd0452a43e
ChanYoungHan <sanbon0812@naver.com>
2025-02-06 12:49:29 +0900

    ⚰️ Remove user schemas

--- python-di-webapp/webapp/schemas.py
+++ python-di-webapp/webapp/schemas.py
@@ -4,17 +4,6 @@ from typing import List, Optional
 from datetime import datetime
 
 
-class UserSchema(BaseModel):
-    """Base schema for User model."""
-    id: int
-    created_at: datetime
-    email: EmailStr
-    is_active: bool = True
-    hashed_password: str
-
-    model_config = ConfigDict(from_attributes=True)
-
-
 class UserRequest(BaseModel):
     """Request schema for User creation."""
     email: EmailStr
